584ab9726697dd67c3d07f7f736df0bb6d015882
Xin Xu <xuxin1999102@126.com>
2023-11-21 19:33:41 +0800

    Update roberta.sh

--- bias_tracing/scripts/roberta.sh
+++ bias_tracing/scripts/roberta.sh
@@ -1,1 +1,1 @@
-python experiments/causal_trace.py --model_name=roberta-large --bias_file=data/stereoset/domain/gender.json
+python experiments/bias_trace.py --model_name=roberta-large --bias_file=data/stereoset/domain/gender.json
