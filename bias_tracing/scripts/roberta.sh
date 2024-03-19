e8094fa816be97e347950714dfce3c4df0e4941f
Xin Xu <xuxin1999102@126.com>
2024-03-19 21:25:17 +0800

    Init

---
+++ bias_tracing/scripts/roberta.sh
@@ -0,0 +1,2 @@
+python experiments/bias_trace.py --model_name=roberta-large --bias_file=data/stereoset/domain/gender.json
+python experiments/bias_trace.py --model_name=roberta-large --bias_file=data/stereoset/domain/raceg.json
